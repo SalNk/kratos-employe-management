013614ad4410720471c44f05a021ff086cab1627
SALEM <salemnk02@gmail.com>
2023-12-25 15:30:47 +0100

    Fill method in Repositories

--- app/Repository/Department/DepartmentRepo.php
+++ app/Repository/Department/DepartmentRepo.php
@@ -8,26 +8,41 @@ class DepartmentRepo implements DepartmentContract
 {
     public function toAdd(array $inputs)
     {
+        $department = Departement::create($inputs);
 
+        return $department;
     }
 
     public function toUpdate(array $inputs, $id)
     {
+        $department = $this->toGetById($id);
 
+        foreach ($inputs as $property => $value)
+            $department->$property = $value;
+        $department->update();
+
+        return $department;
     }
 
     public function toDelete($id)
     {
+        $department = $this->toGetById($id);
+        $department->delete();
 
+        return $department;
     }
 
-    public function toGetAll($n = 50)
+    public function toGetAll($n = 10)
     {
+        $departments = Departement::paginate(10);
 
+        return $departments;
     }
 
     public function toGetById($id)
     {
+        $department = Departement::findOrFail($id);
 
+        return $department;
     }
 }
--- app/Repository/Worker/WorkreRepo.php
+++ app/Repository/Worker/WorkreRepo.php
@@ -1,31 +1,47 @@
 <?php
 
 namespace App\Repository\Worker;
+use App\Models\Employe;
 
 class WorkerRepo implements Workercontract
 {
     public function toAdd(array $inputs)
     {
+        $worker = Employe::create($inputs);
 
+        return $worker;
     }
 
     public function toUpdate(array $inputs, $id)
     {
+        $worker = $this->toGetById($id);
 
+        foreach ($inputs as $property => $value)
+            $worker->$property = $value;
+        $worker->update();
+
+        return $worker;
     }
 
     public function toDelete($id)
     {
+        $worker = $this->toGetById($id);
+        $worker->delete();
 
+        return $worker;
     }
 
-    public function toGetAll($n = 50)
+    public function toGetAll($n = 10)
     {
+        $workers = Employe::paginate(10);
 
+        return $workers;
     }
 
     public function toGetById($id)
     {
+        $worker = Employe::findOrFail($id);
 
+        return $worker;
     }
 }
